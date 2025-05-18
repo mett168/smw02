846b8642421c4bd39d30240b5543fb576f6230ba
METT <cuihongyan188@gmail.com>
2025-05-19 05:43:19 +0900

    ✅ fix: 빌드 오류 해결 위해 InvitePage 타입 제거

--- src/app/invite/[code]/page.tsx
+++ src/app/invite/[code]/page.tsx
@@ -1,16 +1,9 @@
-// 📄 src/app/invite/[code]/page.tsx
-
 import InviteRedirectClient from "./InviteRedirectClient";
 
-interface InvitePageProps {
-  params: {
-    code: string;
-  };
-}
+export default function InvitePage(props: any) {
+  const code = props?.params?.code;
 
-// ✅ 타입을 확정하고 `async` 제거 (Server Component에서 client 컴포넌트로 넘김)
-export default function InvitePage({ params }: InvitePageProps) {
-  return <InviteRedirectClient code={params.code} />;
+  return <InviteRedirectClient code={code} />;
 }
 
 
